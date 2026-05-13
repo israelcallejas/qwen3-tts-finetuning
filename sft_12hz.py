e36c073a24435e243bd047bb08cfe1f03d2f662b
Israel <35155177+israelcallejas@users.noreply.github.com>
2026-05-13 08:41:29 +0200

    fix: missing speaker_encoder

--- sft_12hz.py
+++ sft_12hz.py
@@ -1,16 +1,23 @@
 def train():
-  # (...)
-  config = AutoConfig.from_pretrained(MODEL_PATH)
-  if qwen3tts.model.speaker_encoder is None:
-          base_path = r"C:\Qwen3-TTS\Qwen3-TTS-12Hz-1.7B-Base" # or 0.6B
-          base_model = Qwen3TTSModel.from_pretrained(
-              base_path, 
-              torch_dtype=torch.bfloat16
-          )
-          qwen3tts.model.speaker_encoder = base_model.model.speaker_encoder
-          del base_model
-          import gc
-          gc.collect()
-          torch.cuda.empty_cache()
-  train_data = open(args.train_jsonl).readlines()
-  # (...)
+    # (...)
+    config = AutoConfig.from_pretrained(MODEL_PATH)
+    if qwen3tts.model.speaker_encoder is None:
+        base_path = r"C:\Qwen3-TTS\Qwen3-TTS-12Hz-1.7B-Base" # or 0.6B
+        base_model = Qwen3TTSModel.from_pretrained(
+            base_path, 
+            torch_dtype=torch.bfloat16
+        )
+        qwen3tts.model.speaker_encoder = base_model.model.speaker_encoder
+        del base_model
+        import gc
+        gc.collect()
+        torch.cuda.empty_cache()
+    train_data = open(args.train_jsonl).readlines()
+    # (...)
+
+            # (...)
+            #drop_prefix = "speaker_encoder"
+            #keys_to_drop = [k for k in state_dict.keys() if k.startswith(drop_prefix)]
+            #for k in keys_to_drop:
+            #    del state_dict[k]
+            # (...)
